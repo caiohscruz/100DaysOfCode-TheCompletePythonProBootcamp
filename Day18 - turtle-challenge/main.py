1b81b4f62c5e344a8e0b6c48d583189a2d9a62c9
caiohscruz <caiohscruz@hotmail.com>
2022-02-22 19:47:35 -0300

    Add 3rd turtle challenge from Day18

--- Day18 - turtle-challenge/main.py
+++ Day18 - turtle-challenge/main.py
@@ -9,10 +9,33 @@ screen = Screen()
 #     bob.right(90)
 
 # TODO 2: Draw a Dashed Line
-for _ in range(15):
-    bob.forward(10)
-    bob.penup()
-    bob.forward(10)
-    bob.pendown()
+# for _ in range(15):
+#     bob.forward(10)
+#     bob.penup()
+#     bob.forward(10)
+#     bob.pendown()
+
+# TODO 3: Draw polygons
+import random
+
+
+def change_color():
+    red = random.random()
+    blue = random.random()
+    green = random.random()
+
+    bob.color(red, green, blue)
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    change_color()
+    for _ in range(num_sides):
+        bob.forward(100)
+        bob.right(angle)
+
+
+for num_sides in range(3, 13):
+    draw_shape(num_sides)
+
 
 screen.exitonclick()
