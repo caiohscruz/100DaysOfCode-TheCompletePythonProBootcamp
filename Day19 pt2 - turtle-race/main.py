6b4b759ae3a064aa3b7b18ad8240ff63c821bac7
caiohscruz <caiohscruz@hotmail.com>
2022-02-26 12:24:57 -0300

    Add a finish line to the screen

--- Day19 pt2 - turtle-race/main.py
+++ Day19 pt2 - turtle-race/main.py
@@ -2,6 +2,19 @@ from turtle import *
 from NamedTurtle import NamedTurtle
 import random
 
+
+def draw_finish_line(x_cor, screen_height):
+    pencil = Turtle()
+    pencil.setheading(270)
+    line_lenght = 0.9 * screen_height
+    y_cor = line_lenght / 2
+    pencil.penup()
+    pencil.setpos(x_cor, y_cor)
+    pencil.pendown()
+    pencil.write("Finish Line", False, align="left")
+    pencil.forward(line_lenght)
+
+
 screen = Screen()
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
@@ -19,12 +32,16 @@ RACE_DISTANCE = PROPORTION_ROAD_WIDTH * SCREEN_WIDTH
 
 all_turtles = []
 
-x_axis = - SCREEN_WIDTH * PROPORTION_ROAD_WIDTH / 2
+STARTING_POINT_XCOR = - SCREEN_WIDTH * PROPORTION_ROAD_WIDTH / 2
+
+FINISH_LINE_XCOR = STARTING_POINT_XCOR + RACE_DISTANCE
+draw_finish_line(FINISH_LINE_XCOR, SCREEN_HEIGHT)
+
 
 for number in range(0, num_turtles):
     racer = NamedTurtle(color=colors[number], name=names[number])
-    y_axis = - SCREEN_HEIGHT / 2 + (number + 1) * space_between
-    racer.turtle.setpos(x_axis, y_axis)
+    starting_point_ycor = - SCREEN_HEIGHT / 2 + (number + 1) * space_between
+    racer.turtle.setpos(x=STARTING_POINT_XCOR, y=starting_point_ycor)
     racer.turtle.write(racer.name, False, align="right")
     all_turtles.append(racer)
 
@@ -39,7 +56,7 @@ NUM_STEPS = 5
 while not game_is_over:
     racer = random.choice(all_turtles)
     racer.turtle.forward(NUM_STEPS)
-    if racer.turtle.xcor() >= RACE_DISTANCE + x_axis:
+    if racer.turtle.xcor() >= FINISH_LINE_XCOR:
         game_is_over = True
         print(f"The winner was {racer.name}")
         if racer.name == guess:
