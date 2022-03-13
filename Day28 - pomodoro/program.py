f57798c718fd9be00900fc2380fa151ae3d02a50
caiohscruz <caiohscruz@hotmail.com>
2022-03-13 17:17:14 -0300

    Add label and buttons

--- Day28 - pomodoro/program.py
+++ Day28 - pomodoro/program.py
@@ -25,6 +25,16 @@ canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+lbl_text = tk.Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+lbl_text.grid(column=1, row=0)
+
+btn_start = tk.Button(text="Start")
+btn_start.grid(column=0, row=2)
+
+btn_reset = tk.Button(text="Reset")
+btn_reset.grid(column=2, row=2)
+
 
 window.mainloop()
